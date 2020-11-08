06d561accabd3ae231d3838f6448e4d91c0d3537
Lucas <lucas.acdc92@gmail.com>
2020-11-08 19:37:46 -0300

    create Projects-timeline component

---
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+
+
+//projects
+
+
+
+
+const ProjectTimeLine = () => {
+    return (
+        <div id='projects'>
+            
+        </div>
+    )
+}
+
+export default ProjectTimeLine
